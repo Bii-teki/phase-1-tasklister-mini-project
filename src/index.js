b9f21772a0b91199ef2b011253f31899f0e52366
B33 <geoffrey.bii@student.moringaschool.com>
2023-06-22 17:31:16 +0300

    assignment submission

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const info = document.getElementById("new-task-description");
+   const ul = document.getElementById("tasks");
+  document.addEventListener("DOMContentLoaded", () => {
+  
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const li = document.createElement("li"); 
+    const btn = document.createElement("button"); 
+    btn.innerText = "X"; 
+    btn.addEventListener("click", () => {
+      li.remove();
+    });
+    
+    li.innerHTML = info.value; 
+    li.appendChild(btn); 
+    ul.appendChild(li);
+    let user = []; 
+    user.push(li); 
+    console.log(user); 
+    form.reset() 
+  });
 });
+
